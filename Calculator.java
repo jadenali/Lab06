f642447dce190dddf6e3ccea6c7cdfb219d93b60
jadenali <59582420+jadenali@users.noreply.github.com>
2020-03-17 21:55:27 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    private static double Output;
+    private static double Output;                          //adding a comment in Github
 
     public static void main(String [] args) {
         Scanner scn = new Scanner(System.in);
